91fa832bdb88428b5974898dd8208734132ccae5
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-03-30 01:22:21 +0000

    Add some methods to ATVMediaAsset * Add a comparison for compareTitleWith: * Add title and mediaType rw accessors

--- trunk/ATVMediaAsset.h
+++ trunk/ATVMediaAsset.h
@@ -8,12 +8,23 @@
 
 #import <Cocoa/Cocoa.h>
 #import <BRSimpleMediaAsset.h>
+#import <BRMediaType.h>
 
 @interface ATVMediaAsset : BRSimpleMediaAsset {
 	BOOL _directory;
+  NSString *_title;
+  BRMediaType *_mediaType;
 }
 
 -(BOOL)isDirectory;
 -(void)setDirectory:(BOOL)directory;
+
+-(NSComparisonResult)compareTitleWith:(id)otherAsset;
+
+-(NSString *)title;
+-(void)setTitle:(NSString *)title;
+
+-(BRMediaType *)mediaType;
+-(void)setMediaType:(BRMediaType *)mediaType;
 
 @end
--- trunk/ATVMediaAsset.m
+++ trunk/ATVMediaAsset.m
@@ -19,4 +19,23 @@
 	_directory = directory;
 }
 
+-(NSComparisonResult)compareTitleWith:(id)otherAsset {
+  return [[self title] compare:[otherAsset title] options:NSCaseInsensitiveSearch];
+}
+
+-(NSString *)title {
+  return _title;
+}
+
+-(void)setTitle:(NSString *)title {
+  _title = title;
+}
+
+-(BRMediaType *)mediaType {
+  return _mediaType;
+}
+
+-(void)setMediaType:(BRMediaType *)mediaType {
+  _mediaType = mediaType;
+}
 @end
