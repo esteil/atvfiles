c2eec2eb6f9d91e0d15c6252ec9a685eeef1556d
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-04-04 04:12:26 +0000

    initial localization stuff

---
+++ trunk/LocalizationStuff.h
@@ -0,0 +1,15 @@
+/*
+ *  LocalizationStuff.h
+ *  ATVFiles
+ *
+ *  Some random macros to deal with string localizaitons.
+ *
+ *  Created by Eric Steil III on 4/4/07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+ 
+#import <BRLocalizedStringManager.h>
+
+// This is our replacement for NSLocalizedString
+#define BRLocalizedString(key, comment) [BRLocalizedStringManager appliance:self localizedStringForKey:key inFile:nil]
