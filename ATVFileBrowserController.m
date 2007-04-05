2cc9aa0b46d42bc885ad2f3ca22ec462265396d0
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-04-05 07:22:09 +0000

    Add some fun debug tests

--- ATVFileBrowserController.m
+++ ATVFileBrowserController.m
@@ -81,6 +81,7 @@
 // 140 140 141 141 139 138 139 138
 -(BOOL)brEventAction:(BREvent *)action {
   static int step = 0;
+  int cmd = 0;
   
   if([action value] == 1) {
     switch(step) {
@@ -106,7 +107,8 @@
         step = ([action usage] == 139) ? step + 1 : 0;
         break;
       case 7:
-        step = ([action usage] == 138) ? step + 1 : 0;
+        step = ([action usage] == 138 || [action usage] == 139) ? step + 1 : 0;
+        if([action usage] == 139) cmd = 1;
         break;
       default:
         step = 0;
@@ -115,22 +117,86 @@
     
     // display it here!
     if(step == 8) {
-      NSString *shortVersion = [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
-      BRAlertController *alert = [BRAlertController alertOfType:0
-                 titled:BRLocalizedString(@"ATVFiles Important Information", "")
-            primaryText:[NSString stringWithFormat:BRLocalizedString(@"Version: %@ (%@)", @"Version tag, param 1 is version, param 2 is build"), shortVersion, [NSNumber numberWithFloat:ATVFilesVersionNumber]]
-          secondaryText:[NSString stringWithFormat:BRLocalizedString(@"Copyright (C) 2007 Eric Steil III (ericiii.net)\n\nSpecial Thanks: alan_quatermain\n\n%s", @"Copyright stuff"), ATVFilesVersionString]
-              withScene:[self scene]];
-      [alert setHasGoBackControl:YES];
-  
-      [_stack pushController:alert];
+      if(cmd == 0) {
+        // easter egg
+        NSString *shortVersion = [[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+        BRAlertController *alert = [BRAlertController alertOfType:0
+                   titled:BRLocalizedString(@"ATVFiles Important Information", "")
+              primaryText:[NSString stringWithFormat:BRLocalizedString(@"Version: %@ (%@)%@", @"Version tag, param 1 is version, param 2 is build"), shortVersion, [NSNumber numberWithFloat:ATVFilesVersionNumber], 
+#ifdef DEBUG
+              @"\nDEBUG BUILD"
+#else
+              @""
+#endif
+            ]
+            secondaryText:[NSString stringWithFormat:BRLocalizedString(@"Copyright (C) 2007 Eric Steil III (ericiii.net)\n\nSpecial Thanks: alan_quatermain\n\n%s", @"Copyright stuff"), ATVFilesVersionString]
+                withScene:[self scene]];
+        [alert setHasGoBackControl:YES];
 
+        [_stack pushController:alert];
+      } else if(cmd == 1) {
+#ifdef DEBUG
+        [self _debugOptionsMenu];
+#endif
+      }
       step = 0;
     }
   }
   
   return [super brEventAction:action];
 }
+
+#ifdef DEBUG
+-(void)_debugOptionsMenu {
+  // stupid diagnostics thing, ONLY ENABLED IN DEBUG MODE
+  BROptionDialog *dialog = [[BROptionDialog alloc] initWithScene:[self scene]];
+  [dialog setTitle:@"Special Secret Sauce"];
+  [dialog setIcon:[self listIcon] horizontalOffset:0 kerningFactor:0];
+  [dialog setPrimaryInfoText:@"Special options, just for fun!"];
+  [dialog setHasGoBackControl:YES];
+
+  [dialog addOptionText:@"remove purple box"];
+  [dialog addOptionText:@"overlay purple box"];
+  [dialog addOptionText:@"Option 2"];
+  [dialog addOptionText:@"Option 3"];
+  [dialog addOptionText:@"Option 4"];
+
+  [dialog setActionSelector:@selector(optionDialogActionSelector:) target:self];
+
+  [_stack pushController:dialog];
+}
+
+-(void)optionDialogActionSelector:(id)evt {
+  int index = [evt selectedIndex];
+  static BRQuadLayer *white;
+  if(index == 0) {
+    if(white) {
+      [white removeFromSuperlayer];
+      [white release];
+      white = nil;
+      [_scene renderScene];
+    }
+  } else if(index == 1) {
+/*    BRLayerController *layer = [BRLayerController layerControllerWithScene:[self scene]];*/
+
+/*    [[layer masterLayer] setAlphaValue:0.5];
+    [[layer masterLayer] setFrame:NSMakeRect(0, 0, 900, 200)];
+*/  
+    if(!white) {
+      white = [BRQuadLayer layerWithScene:[self scene]];
+      [white retain];
+      [[_scene root] insertSublayer:white above:[self masterLayer]];
+    }
+    
+    [white setRedColor:1.0 greenColor:0.0 blueColor:1.0];
+    [white setFrame:NSMakeRect(50, 50, 500, 500)];
+    [white setAlphaValue:0.5];
+    [_scene renderScene];
+
+/*    [_stack pushController:layer];*/
+  }
+}
+#endif
 
 // this is called before redrawing it after something else has been shown.
 // refresh the directory here.
