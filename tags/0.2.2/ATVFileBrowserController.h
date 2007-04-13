f641d8632b27fb593fcb502b8b4f8d6c71e4236e
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-04-13 19:34:24 +0000

    tag 0.2.2

---
+++ tags/0.2.2/ATVFileBrowserController.h
@@ -0,0 +1,35 @@
+//
+//  ATVFileBrowserController.h
+//  ATVFiles
+//  
+//  This is the primary menu controller for browsing files.
+//
+//  Created by Eric Steil III on 3/29/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <BackRow/BackRow.h>
+
+#import "ATVDirectoryContents.h"
+
+extern const double ATVFilesVersionNumber;
+extern const unsigned char ATVFilesVersionString[];
+
+@interface ATVFileBrowserController : BRMediaMenuController {
+  NSString *_directory;
+  ATVDirectoryContents *_contents;
+#ifdef DEBUG
+  BRTextLayer *_debugTag;
+#endif
+}
+
+-(ATVFileBrowserController *)initWithScene:(id)scene forDirectory:(NSString *)directory;
+-(ATVFileBrowserController *)initWithScene:(id)scene forDirectory:(NSString *)directory useFolderNameForTitle:(BOOL)useFolderName;
+
+#ifdef DEBUG
+-(void)_debugOptionsMenu;
+-(void)_addDebugTag;
+-(void)_removeDebugTag;
+#endif
+@end
