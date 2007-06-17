8fcf8fa6afdab62ede1311da10e4a46b0aa96bb1
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-06-17 18:27:02 +0000

    Add stub classes for fullscreen app controller and vlc app controller

---
+++ ATVFVLCAppController.m
@@ -0,0 +1,14 @@
+//
+//  ATVFVLCAppController.m
+//  ATVFiles
+//
+//  Created by Eric Steil III on 6/17/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "ATVFVLCAppController.h"
+
+
+@implementation ATVFVLCAppController
+
+@end
---
+++ ATVFullscreenAppController.m
@@ -0,0 +1,14 @@
+//
+//  ATVFullscreenAppController.m
+//  ATVFiles
+//
+//  Created by Eric Steil III on 6/17/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "ATVFullscreenAppController.h"
+
+
+@implementation ATVFullscreenAppController
+
+@end
