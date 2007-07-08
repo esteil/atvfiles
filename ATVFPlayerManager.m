355c8cedf9730189a46f2c46993421a508d96732
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-07-08 21:30:02 +0000

    Use the proper player type based on the file extensions

---
+++ ATVFPlayerManager.m
@@ -0,0 +1,59 @@
+//
+//  ATVFPlayerManager.m
+//  ATVFiles
+//
+//  Created by Eric Steil III on 7/8/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "ATVFPlayerManager.h"
+#import "ATVFilesAppliance.h"
+
+@implementation ATVFPlayerManager
+
++(ATVFMusicPlayer *)musicPlayer {
+  static ATVFMusicPlayer *_musicPlayer;
+  
+  // initialize our player
+  if(!_musicPlayer) {
+    _musicPlayer = [[[ATVFMusicPlayer alloc] init] retain];
+  }
+  
+  return _musicPlayer;
+}
+
++(BRQTKitVideoPlayer *)videoPlayer {
+  return [[[BRQTKitVideoPlayer alloc] init] autorelease];
+}
+
++(id)playerForType:(enum ATVFPlayerType)type {
+  switch(type) {
+    case kATVFPlayerMusic:
+      return [self musicPlayer];
+      break;
+    case kATVFPlayerVideo:
+      return [self videoPlayer];
+      break;
+    default:
+      return nil;
+      break;
+  }
+}
+
+// return the player type we need, this only looks at the asset's extension for now
++(enum ATVFPlayerType)playerTypeForAsset:(ATVMediaAsset *)asset {
+  NSArray *videoExtensions = [[NSUserDefaults standardUserDefaults] arrayForKey:kATVPrefVideoExtensions];
+  NSArray *audioExtensions = [[NSUserDefaults standardUserDefaults] arrayForKey:kATVPrefAudioExtensions];
+  
+  NSString *extension = [[[asset mediaURL] pathExtension] lowercaseString];
+  
+  if([videoExtensions containsObject:extension]) {
+    return kATVFPlayerVideo;
+  } else if([audioExtensions containsObject:extension]) {
+    return kATVFPlayerMusic;
+  } else {
+    return kATVFPlayerVideo;
+  }
+}
+
+@end
