e4aad03c5c674c9a45f5849a9d8d4a61cb17d8c5
steile <steile@ee79a50e-9c42-0410-a672-5142589a767e>
2007-12-29 08:26:10 +0000

    GPL3 license

--- trunk/ATVFullscreenAppController.m
+++ trunk/ATVFullscreenAppController.m
@@ -1,9 +1,22 @@
 //
-//  ATVFullscreenAppController.m
-//  ATVFiles
+// ATVFullscreenAppController.m
+// ATVFiles
 //
-//  Created by Eric Steil III on 6/17/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+// Created by Eric Steil III on 6/17/07.
+// Copyright (C) 2007 Eric Steil III
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #ifdef ENABLE_EXTERNAL_PLAYERS
