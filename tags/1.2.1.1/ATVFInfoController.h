c26700166cddd32e6a6fed24c6523de3bbc7ac46
esteil <esteil@ee79a50e-9c42-0410-a672-5142589a767e>
2009-11-01 18:06:23 +0000

    Tag 1.2.1.1

---
+++ tags/1.2.1.1/ATVFInfoController.h
@@ -0,0 +1,37 @@
+//
+// ATVFInfoController.h
+// ATVFiles
+//
+// Created by Eric Steil III on 9/15/07.
+// Copyright (C) 2007-2008 Eric Steil III
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <BackRow/BackRow.h>
+#import <ATVFMediaAsset.h>
+#import <SapphireCompatClasses/SapphireLayerController.h>
+
+@interface ATVFInfoController : SapphireLayerController {
+  BRHeaderControl *_header;
+  BRVerticalScrollControl *_document;
+  ATVFMediaAsset *_asset;
+}
+
+-(void)doLayout;
+-(void)setAsset:(ATVFMediaAsset *)asset;
+-(ATVFMediaAsset *)asset;
+
+@end
